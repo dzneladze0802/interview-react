35a66d40f031b4f27d1bb953e27876b666c723b1
Davit Dzneladze <dzneladze0802@gmail.com>
2025-01-06 00:38:12 +0100

    curry update

--- src/JS/Middle/curry.js
+++ src/JS/Middle/curry.js
@@ -2,6 +2,6 @@ function curry() {
   let count = 0;
 }
 
-console.log(curry()()); // 0
-console.log(curry()(2)()); // 2
-console.log(curry()(1)(2)(0)(3)()); // 6
+console.log(curry()); // 0
+console.log(curry(2)()); // 2
+console.log(curry(1)(2)(3)()); // 5
